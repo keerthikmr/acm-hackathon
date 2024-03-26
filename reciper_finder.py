3735db921d6eee64579c736b500351b39e8db877
TerminalC3 <keerthikumar.m10@gmail.com>
2024-03-26 19:41:09 +0530

    Styling

--- reciper_finder.py
+++ reciper_finder.py
@@ -1,53 +1,53 @@
-import requests
+# import requests
 
-API_KEY = '60fdfcff605c48379d95f8dec1739b0f'
-ENDPOINT = 'https://api.spoonacular.com/recipes/findByIngredients'
+# API_KEY = '60fdfcff605c48379d95f8dec1739b0f'
+# ENDPOINT = 'https://api.spoonacular.com/recipes/findByIngredients'
 
-ingredients = ['chicken', 'rice', 'broccoli']
+# ingredients = ['chicken', 'rice', 'broccoli']
 
-def get_recipes(ingredients):
+# def get_recipes(ingredients):
 
-    params = {
-        'apiKey': API_KEY,
-        'ingredients': ','.join(ingredients),
-        'number': 5,
-    }
+#     params = {
+#         'apiKey': API_KEY,
+#         'ingredients': ','.join(ingredients),
+#         'number': 5,
+#     }
 
-    response = requests.get(ENDPOINT, params=params)
+#     response = requests.get(ENDPOINT, params=params)
     
-    if response.status_code == 200:
-        recipes = response.json()
-        print(recipes)
-        print("\n")
-        recipe_list = []
+#     if response.status_code == 200:
+#         recipes = response.json()
+#         print(recipes)
+#         print("\n")
+#         recipe_list = []
 
-        params_inst = {
-            'apiKey': API_KEY,
-        }
-        
-        recipe_dict = {}
+#         params_inst = {
+#             'apiKey': API_KEY,
+#         }
 
-        for recipe in recipes:
-            steps_list = []
+#         recipe_dict = {}
 
-            recipe_name = recipe['title']
+#         for recipe in recipes:
+#             steps_list = []
+
+#             recipe_name = recipe['title']
 
 
-            INST = f'https://api.spoonacular.com/recipes/{recipe["id"]}/analyzedInstructions'
+#             INST = f'https://api.spoonacular.com/recipes/{recipe["id"]}/analyzedInstructions'
             
-            inst_response = requests.get(INST, params=params_inst).json()
-            print("RESPONSE")
-            print(inst_response)
-            for step in inst_response[0]['steps']:
-                steps_list.append(step['step'])
+#             inst_response = requests.get(INST, params=params_inst).json()
+#             print("RESPONSE")
+#             print(inst_response)
+#             for step in inst_response[0]['steps']:
+#                 steps_list.append(step['step'])
 
-            recipe_dict[recipe_name] = steps_list
-            print("inner----->")
-            print(recipe_dict)
-        print(recipe_dict)
-        return recipe_dict
+#             recipe_dict[recipe_name] = steps_list
+#             print("inner----->")
+#             print(recipe_dict)
+#         print(recipe_dict)
+#         return recipe_dict
 
-    else:
-        print('Error fetching recipes:', response.status_code)
+#     else:
+#         print('Error fetching recipes:', response.status_code)
 
-# get_recipes(ingredients)
+# # get_recipes(ingredients)
