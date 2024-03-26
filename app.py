3457eee4590e5486ca8c89b7ffeb75798fb4c5c9
TerminalC3 <keerthikumar.m10@gmail.com>
2024-03-27 04:32:26 +0530

    Add warnings

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import reciper_finder, get_response
-import json
+import recipe_finder, get_response
 
 app = Flask(__name__)
 
@@ -9,21 +8,21 @@ inventory1=[]
 
 sel_inventory =[]
 
-ingredient_list = [['Apple', 4], ['Orange', 5]]
+ingredient_list = [['Apple', 2, 2], ['Orange', 5, 4]]
 
 @app.route('/')
 def index():
-	return render_template('inventory.html')
+	return render_template('inventory.html', ingredients=ingredient_list)
 
 @app.route('/recipe', methods=['GET', 'POST'])
 def recipe():
     if request.method == 'POST':
         items = request.form.get('ingredient')
         items = items.split(',')
-        # return render_template('recipe.html', recipe=reciper_finder.get_recipes(items))
+        return render_template('recipe.html', recipe=recipe_finder.get_recipes(items))
 
-        return render_template('recipe.html', recipe=
-[{'Chicken Arrozcaldo': {'steps': ['In a large soup pot over medium heat, saute garlic in olive oil until light brown.', 'Add ginger and onion, and saut for a minute.', 'Add chicken cube and stir until it melts.', 'Add chicken and stir-fry until both sides turn light brown.', 'Add fish sauce and simmer for another minute.', 'Add rice and water.', 'Mix well. Cover and simmer in medium heat for about 40 minutes or until the rice is fully cooked. Stir occasionally.', 'Put-in the hard boiled eggs', 'Add the safflower for additional color and aroma.', 'Stir in a small amount of sliced green onions.', 'Add salt and pepper if necessary.', 'If rice soup becomes too thick, add a little water to thin it a bit.', 'Garnish each bowl serving with sliced green onions and fried garlic just before serving.', 'Sprinkle a little lemon juice on the soup servings. (optional)'], 'used': ['rice', 'add chicken and stir until it melts', 'add rice and water. mix well. cover and simmer in heat'], 'unused': ['garlic', 'onion', 'thumb sized ginger', 'chicken cube', 'fish sauce', 'green onion', 'hardboiled quail eggs', 'pcs lemon', 'in a soup pot over heat', 'stir in a amount of green onions. add salt and pepper if necessary', 'sprinkle a little lemon juice on the soup']}, 'Steamy Creamy Mushroom Risotto': {'steps': ['Fry the chicken and the mushrooms and set aside.Fry the rice in a pan in the oil for a minute. Start boiling water and prepare the chicken broth in a separate saucepan.Start adding chicken broth water and the wine little by little to the rice and simmer at medium heat until the rice is cooked.Once the rice is done, add the butter, the chicken, and the mushrooms and blend them in.', 'Add the parmesan, salt, and pepper and stir one last time before you serve.'], 'used': ['chicken', 'rice'], 'unused': ['butter', 'chicken broth', 'mushrooms', 'onion', 'parmesan', 'bell pepper', 'white wine']}, 'Light and Chunky Chicken Soup': {'steps': ['Place the chicken in a pot and add enough water to cover the chicken. Three quarters of the pot full.', 'Add the onion, carrot bits, salt pepper, rosemary, basil.Bring to the boil and allow to simmer until the chicken starts to pull away from the bone.', 'Remove the chicken from the pot and let it cool. Strip the meat off in tiny bite sized pieces.In the meantime, add the cup of rice to the stock and let it simmer for another 10 - 15 minutes. The rice must be well cooked.', 'Add the celery and the chicken to the soup.Allow to simmer for another 10 minutes.', 'Garnish with parsley.Ready to serve with some crispy hot bread or croutons on the side.'], 'used': ['basmati rice', 'chicken'], 'unused': ['carrot', 'celery finelly', 'basil', 'rosemary', 'parsley', 'a onion', 'bell pepper']}, 'SLOW COOKER CHICKEN GUMBO SOUP': {'steps': ['Put all ingredients in crockpot and cook on high for 4 hours or low for 6 to 7 hours.'], 'used': ['chicken', 'rice'], 'unused': ['can chicken broth', 'carrot', 'bell pepper', 'onion', 'canned tomatoes', 'okra', 'cajun seasoning', 'hot sauce']}, 'Hydrabadi Dum Biriyani': {'steps': ['Marinate the meat with curd, turmeric powder, biriyani masala, ginger garlic paste and salt overnight.', 'Heat ghee in a thick bottom pan add chicken and fry and cook meat until tender. Set meat aside', 'Add ghee, saute whole garam masala and cook rice until 90%% done; add salt and little saffrom milk for colour.', 'Remove from the flame when rice is cooked and no moisture is left.', 'In a thick-bottomed vessel, add the first layer of rice, half of the mint leaves, coriander and fried onions; pour ghee and saffron milk over it and cover the layer of rice with the meat.', 'Garnish with mint leaves, fried onions and coriander and saffron milk.', 'Cover the dish with foil and cover with heavy lid and cook on dum for 30-40 mins on medium heat.'], 'used': ['basmati rice', 'chicken biriyani masala'], 'unused': ['leg pieces', 'onions', 'garam masala', 'turmeric powder', 'ginger garlic paste', 'coriander & mint leaves', 'ghee as required', 'saffron strands dissolved warm milk', 'egg & fried onions']}}, ['Apple', 'Orange']])
+#         return render_template('recipe.html', recipe=
+# [{'Chicken Arrozcaldo': {'steps': ['In a large soup pot over medium heat, saute garlic in olive oil until light brown.', 'Add ginger and onion, and saut for a minute.', 'Add chicken cube and stir until it melts.', 'Add chicken and stir-fry until both sides turn light brown.', 'Add fish sauce and simmer for another minute.', 'Add rice and water.', 'Mix well. Cover and simmer in medium heat for about 40 minutes or until the rice is fully cooked. Stir occasionally.', 'Put-in the hard boiled eggs', 'Add the safflower for additional color and aroma.', 'Stir in a small amount of sliced green onions.', 'Add salt and pepper if necessary.', 'If rice soup becomes too thick, add a little water to thin it a bit.', 'Garnish each bowl serving with sliced green onions and fried garlic just before serving.', 'Sprinkle a little lemon juice on the soup servings. (optional)'], 'used': ['rice', 'add chicken and stir until it melts', 'add rice and water. mix well. cover and simmer in heat'], 'unused': ['garlic', 'onion', 'thumb sized ginger', 'chicken cube', 'fish sauce', 'green onion', 'hardboiled quail eggs', 'pcs lemon', 'in a soup pot over heat', 'stir in a amount of green onions. add salt and pepper if necessary', 'sprinkle a little lemon juice on the soup']}, 'Steamy Creamy Mushroom Risotto': {'steps': ['Fry the chicken and the mushrooms and set aside.Fry the rice in a pan in the oil for a minute. Start boiling water and prepare the chicken broth in a separate saucepan.Start adding chicken broth water and the wine little by little to the rice and simmer at medium heat until the rice is cooked.Once the rice is done, add the butter, the chicken, and the mushrooms and blend them in.', 'Add the parmesan, salt, and pepper and stir one last time before you serve.'], 'used': ['chicken', 'rice'], 'unused': ['butter', 'chicken broth', 'mushrooms', 'onion', 'parmesan', 'bell pepper', 'white wine']}, 'Light and Chunky Chicken Soup': {'steps': ['Place the chicken in a pot and add enough water to cover the chicken. Three quarters of the pot full.', 'Add the onion, carrot bits, salt pepper, rosemary, basil.Bring to the boil and allow to simmer until the chicken starts to pull away from the bone.', 'Remove the chicken from the pot and let it cool. Strip the meat off in tiny bite sized pieces.In the meantime, add the cup of rice to the stock and let it simmer for another 10 - 15 minutes. The rice must be well cooked.', 'Add the celery and the chicken to the soup.Allow to simmer for another 10 minutes.', 'Garnish with parsley.Ready to serve with some crispy hot bread or croutons on the side.'], 'used': ['basmati rice', 'chicken'], 'unused': ['carrot', 'celery finelly', 'basil', 'rosemary', 'parsley', 'a onion', 'bell pepper']}, 'SLOW COOKER CHICKEN GUMBO SOUP': {'steps': ['Put all ingredients in crockpot and cook on high for 4 hours or low for 6 to 7 hours.'], 'used': ['chicken', 'rice'], 'unused': ['can chicken broth', 'carrot', 'bell pepper', 'onion', 'canned tomatoes', 'okra', 'cajun seasoning', 'hot sauce']}, 'Hydrabadi Dum Biriyani': {'steps': ['Marinate the meat with curd, turmeric powder, biriyani masala, ginger garlic paste and salt overnight.', 'Heat ghee in a thick bottom pan add chicken and fry and cook meat until tender. Set meat aside', 'Add ghee, saute whole garam masala and cook rice until 90%% done; add salt and little saffrom milk for colour.', 'Remove from the flame when rice is cooked and no moisture is left.', 'In a thick-bottomed vessel, add the first layer of rice, half of the mint leaves, coriander and fried onions; pour ghee and saffron milk over it and cover the layer of rice with the meat.', 'Garnish with mint leaves, fried onions and coriander and saffron milk.', 'Cover the dish with foil and cover with heavy lid and cook on dum for 30-40 mins on medium heat.'], 'used': ['basmati rice', 'chicken biriyani masala'], 'unused': ['leg pieces', 'onions', 'garam masala', 'turmeric powder', 'ginger garlic paste', 'coriander & mint leaves', 'ghee as required', 'saffron strands dissolved warm milk', 'egg & fried onions']}}, ['Apple', 'Orange']])
 
     else:
          return render_template('recipe.html', recipe="")
@@ -57,38 +56,45 @@ def some():
                         if ingre[0] == red_ingre[0]:
                             ingre[1] = int(ingre[1]) - int(red_ingre[1])
 
-             return render_template('useItems.html', ingredients=converted_list)
-     
+             return render_template('inventory.html', ingredients=ingredient_list)
 
 @app.route('/inventory')
 def inventory():
-    return render_template('inventory.html', ingredients=inventory1)
+    return render_template('inventory.html', ingredients=ingredient_list)
 
 @app.route('/add_ingredient', methods=['POST'])
 def add_ingredient():
+    global ingredient_list
     new_ingredient = request.form.get('newIngredient')
     quantity = int(request.form.get('quantity'))
-    ingre = new_ingredient + ", " + str(quantity)
+    expiration = int(request.form.get('expiration'))
+
+    ingre = [new_ingredient, int(quantity), int(expiration)]
     
-    inventory1.append(ingre)
-    print('inven')
-    print(inventory1)
-    return redirect(url_for('inventory'))
+    ingredient_list.append(ingre)
+
+    return render_template('inventory.html', ingredients=ingredient_list)
 
 @app.route('/add_to_inventory', methods=['POST'])
 def add_to_inventory():
     selected_ingredients = request.form.getlist('ingredient')
-    print(list(selected_ingredients))
-    ing_list = [] 
+    print("ingreeee", selected_ingredients)
+
+    ing_list = []
     for ing in selected_ingredients:
-         ing_list.append(ing.split(', '))
 
-    print("this")
-    print(ing_list)
-    sel_inventory = selected_ingredients
+         ing_list.append(''.join(letter for letter in ing if letter.isalpha()))
+
     print("Selected ingredients:", ing_list)
-    return redirect(url_for('inventory'))
 
+    # ingredient_list = []
+    # for i in ing_list:
+    #     ingredient_list.append(i[0])
+    # ingredient_list = ing_list    
+    # print(ingredient_list)
+
+    return render_template('recipe.html', recipe=recipe_finder.get_recipes(ing_list))
+    # return render_template('recipe.html', recipe="")
 
 if __name__ == '__main__':
     app.debug = True
---
+++ recipe_finder.py
@@ -0,0 +1,60 @@
+import requests
+
+API_KEY = '2eeab4c007dd414a84d729ef80c1f215'
+ENDPOINT = 'https://api.spoonacular.com/recipes/findByIngredients'
+
+ingredients = ['chicken', 'rice', 'broccoli']
+
+def get_recipes(ingredients):
+
+    params = {
+        'apiKey': API_KEY,
+        'ingredients': ','.join(ingredients),
+        'number': 5,
+    }
+
+    response = requests.get(ENDPOINT, params=params)
+    
+    if response.status_code == 200:
+        recipes = response.json()
+
+        params_inst = {
+            'apiKey': API_KEY,
+        }
+
+        recipe_dict = {}
+
+        for recipe in recipes:
+            steps_list = []
+            used_ing = []
+            unused_ing = []
+
+            recipe_name = recipe['title']
+
+            INST = f'https://api.spoonacular.com/recipes/{recipe["id"]}/analyzedInstructions'
+            
+            inst_response = requests.get(INST, params=params_inst).json()
+
+            for step in inst_response[0]['steps']:
+                steps_list.append(step['step'])
+            
+            unused = recipe['missedIngredients']
+            used = recipe['usedIngredients']
+
+            for ing in used:
+                used_ing.append(ing['originalName'])
+            
+            for ing in unused:
+                unused_ing.append(ing['originalName'])
+
+            step_used_unused = {'steps': steps_list, 'used': used_ing, 'unused': unused_ing}
+
+            recipe_dict[recipe_name] = step_used_unused
+
+        print(recipe_dict)
+        return [recipe_dict, ingredients]
+
+    else:
+        print('Error fetching recipes:', response.status_code)
+
+# get_recipes(ingredients)
